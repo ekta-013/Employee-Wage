c74fe691c76bd9baa31133267dcdc64817f6384c
ekta <ektamo0123@gmail.com>
2022-08-25 03:45:59 +0530

    Added : monthly wage calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,32 +8,33 @@ public class EmployeeWage {
         final int IS_PARTTIME = 2;
         final int WAGE_PER_HR = 20;
         final int NUM_OF_WORKING_DAYS = 20;
+        final int MAX_WORKING_HRS = 100;
         
-        int EMP_HRS=0;
-        int empWage=0;
-        int totalEmpWage=0;
-
-        for (int day=0; day<NUM_OF_WORKING_DAYS; day++);{
+        int workingHrs = 0; 
+        int totalWorkingHrs = 0; 
+        int totalWorkingDays = 0;
+        while (totalWorkingHrs <= MAX_WORKING_HRS && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
         switch(empCheck) {
             case IS_FULLTIME:
-                EMP_HRS = 8;
+            	workingHrs = 8;
                 System.out.println("Employee is full time");
             break;
             
             case IS_PARTTIME:
-                EMP_HRS = 4;
+            	workingHrs = 4;
                 System.out.println("Employee is part time");
             break;
             default:
-                EMP_HRS = 0;
+            	workingHrs = 0;
                 System.out.println("Employee is absent");
         }
-        empWage = EMP_HRS * WAGE_PER_HR;
-        totalEmpWage +=empWage;
-        System.out.println("Employee Wage is: "+empWage);
-	}
-        System.out.println("total employee wage: "+totalEmpWage);
-	}
+        totalWorkingHrs += workingHrs;
+        System.out.println("Day " + totalWorkingDays + " Employee working hours= " + workingHrs);
+    }
+    int totalEmpWage = totalWorkingHrs * WAGE_PER_HR;
+    System.out.println("Total Emp Wage: " + totalEmpWage);
+}
 }
